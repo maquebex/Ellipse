0c4e59d06b514c71f103da3aafeda04af0f27386
Pinaki Sinha <spinaki@gmail.com>
2015-12-14 15:44:05 -0800

    added aar links. crashing on run because of transitive dependency in library

--- krumbs_reacho/Android/KrumbsReacho/KrumbsSDKSampleApp/src/main/java/com/krumbs/getkrumbs/sdk/reachoapp/PostFormItemActivity.java
+++ krumbs_reacho/Android/KrumbsReacho/KrumbsSDKSampleApp/src/main/java/com/krumbs/getkrumbs/sdk/reachoapp/PostFormItemActivity.java
@@ -1,17 +1,22 @@
 package com.krumbs.getkrumbs.sdk.reachoapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.FontAwesomeIcons;
+
+import io.krumbs.context.intent.ExpressionalIntent;
+import io.krumbs.krumbscamera.adapter.ContextSwitchAdapter;
+import io.krumbs.krumbscamera.camera.KCameraFrgment;
 
 public class PostFormItemActivity extends AppCompatActivity {
 
@@ -58,7 +63,14 @@ public class PostFormItemActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 System.out.println("Start SDK Camera");
-
+                FragmentTransaction ft =
+                        getSupportFragmentManager().beginTransaction();
+                ExpressionalIntent expressionalIntent = new ExpressionalIntent();
+                ContextSwitchAdapter contextSwitchAdapter = new ContextSwitchAdapter(null, null, null);
+                System.out.println("End SDK Camera");
+                KCameraFrgment kCameraFrgment = new KCameraFrgment();
+//                KCameraFrgment kCameraFrgment = new KCameraFrgment();
+//                ft.add(R.id.fullscreen_content, kCameraFrgment);
             }
 
         });
