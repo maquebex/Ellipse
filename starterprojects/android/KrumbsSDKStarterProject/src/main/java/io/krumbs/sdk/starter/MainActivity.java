37b1464cbc6febedfe3c6ff07350cc5f3645090c
Asquith Bailey <asquith@gmail.com>
2016-01-24 11:13:33 -0800

    Updating sample app resource files so they load correctly with resources from asset-generator tool.

--- starterprojects/android/KrumbsSDKStarterProject/src/main/java/io/krumbs/sdk/starter/MainActivity.java
+++ starterprojects/android/KrumbsSDKStarterProject/src/main/java/io/krumbs/sdk/starter/MainActivity.java
@@ -5,28 +5,68 @@
  */
 package io.krumbs.sdk.starter;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewManager;
 
-import java.io.IOException;
+import java.util.Map;
 
 import io.krumbs.sdk.KrumbsSDK;
+import io.krumbs.sdk.krumbscapture.KCaptureCompleteListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+  private static final String[] PERMISSIONS_LOCATION = {Manifest.permission.ACCESS_COARSE_LOCATION,
+          Manifest.permission.ACCESS_FINE_LOCATION};
+  private static final int REQUEST_LOCATION = 1;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
+    final View startCaptureButton = findViewById(R.id.kcapturebutton);
+    final View helloText = findViewById(R.id.hello_world);
+    startCaptureButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+//        NavUtils.navigateUpFromSameTask(MainActivity.this);
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+          requestLocationPermission();
+        } else {
+          startCaptureButton.setVisibility(View.INVISIBLE);
+          helloText.setVisibility(View.INVISIBLE);
+          startCamera();
+        }
+      }
+    });
   }
+
+
+  private void startCamera() {
+    int containerId = R.id.camera_container;
+    KrumbsSDK.startCapture(containerId, this, new KCaptureCompleteListener() {
+      @Override
+      public void captureCompleted(CompletionState completionState, boolean b, Map<String, Object> map) {
+        View cameraView = findViewById(R.id.camera_container);
+
+      }
+    });
+  }
+
+  private void requestLocationPermission() {
+    ActivityCompat.requestPermissions(this, PERMISSIONS_LOCATION, REQUEST_LOCATION);
+  }
+
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
--- starterprojects/android/KrumbsSDKStarterProject/src/main/java/io/krumbs/sdk/starter/StarterApplication.java
+++ starterprojects/android/KrumbsSDKStarterProject/src/main/java/io/krumbs/sdk/starter/StarterApplication.java
@@ -9,36 +9,68 @@ import android.app.Application;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 
-import java.io.IOException;
 
+import io.krumbs.sdk.KIntentPanelConfiguration;
 import io.krumbs.sdk.KrumbsSDK;
+import io.krumbs.sdk.KrumbsUser;
 
 
 public class StarterApplication extends Application {
     public static final String KRUMBS_SDK_APPLICATION_ID = "io.krumbs.sdk.APPLICATION_ID";
     public static final String KRUMBS_SDK_CLIENT_KEY = "io.krumbs.sdk.CLIENT_KEY";
+    public static final String SDK_STARTER_PROJECT_USER_FN = "JohnQ";
+    public static final String SDK_STARTER_PROJECT_USER_SN = "Public";
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        String appID = StarterApplication.getMetadata(getApplicationContext(), KRUMBS_SDK_APPLICATION_ID);
-        String clientKey = StarterApplication.getMetadata(getApplicationContext(), KRUMBS_SDK_CLIENT_KEY);
+        String appID = getMetadata(getApplicationContext(), KRUMBS_SDK_APPLICATION_ID);
+        String clientKey = getMetadata(getApplicationContext(), KRUMBS_SDK_CLIENT_KEY);
         if (appID != null && clientKey != null) {
             KrumbsSDK.initialize(getApplicationContext(), appID, clientKey);
 
-            String assetPath = "IntentResourcesExample";
             try {
+
+                // Register the Intent Panel model
+                // the emoji image assets will be looked up by name when the KCapture camera is started
+                // Make sure to include the images in your resource directory before starting the KCapture
+                String assetPath = "IntentResourcesExample";
                 KrumbsSDK.registerIntentCategories(assetPath);
-            } catch (IOException e) {
+
+
+                // Configure the IntentPanel View (colors & fonts)
+                KIntentPanelConfiguration defaults = KrumbsSDK.getIntentPanelViewConfigurationDefaults();
+                KIntentPanelConfiguration.IntentPanelCategoryTextStyle ts = defaults.getIntentPanelCategoryTextStyle();
+                ts.setTextColor(Color.YELLOW);
+                KIntentPanelConfiguration newDefaults = new KIntentPanelConfiguration.KIntentPanelConfigurationBuilder()
+                        .intentPanelBarColor("#029EE1")
+                        .intentPanelTextStyle(ts)
+                        .build();
+
+                KrumbsSDK.setIntentPanelViewConfigurationDefaults(newDefaults);
+
+                // Register user information (if your app requires login)
+                // to improve security on the mediaJSON created.
+                String userEmail = DeviceUtils.getPrimaryUserID(getApplicationContext());
+                KrumbsSDK.registerUser(new KrumbsUser.KrumbsUserBuilder()
+                        .email(userEmail)
+                        .firstName(SDK_STARTER_PROJECT_USER_FN)
+                        .lastName(SDK_STARTER_PROJECT_USER_SN).build());
+
+
+
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
     }
 
-    public static String getMetadata(Context context, String name) {
+    public String getMetadata(Context context, String name) {
         try {
             ApplicationInfo appInfo = context.getPackageManager().getApplicationInfo(
                     context.getPackageName(), PackageManager.GET_META_DATA);
